c26ea39dcab8581eb933d0ae51bc3bcd71d879e3
ronniedipple <ronniedipple@gmail.com>
2020-01-07 14:38:00 -0600

    Model added

--- app/src/main/java/com/example/wildfire_fixed_imports/model/User.kt
+++ app/src/main/java/com/example/wildfire_fixed_imports/model/User.kt
@@ -1,11 +1,16 @@
 package com.example.wildfire_fixed_imports.model
 
-data class User (
-    val first_name: String,
-    val last_name: String,
-    val email: String,
-    val username: String,
-    val password: String
+import com.mapbox.mapboxsdk.geometry.LatLng
+import java.util.*
+
+data class User(
+    var id: UUID,
+    var irst_name: String,
+    var last_name: String,
+    var email: String,
+    var cell_number: Int,
+    var recieve_sms: Boolean,
+    var recieve_push: Boolean
 )
 
 data class UserLogin(
@@ -17,4 +22,24 @@ data class UserResponse(
     val username: String,
     val id: Int,
     val token: String
+)
+
+data class FireLocations(
+    var latitude: Float,
+    var longitued: Float,
+    var address: String,
+    var address_label: String,
+    var radius: Int,
+    var last_altert: Int,
+    var notification_timer: Int,
+    var notifications: Boolean
+) {
+    fun getLatLng(): LatLng {
+        return LatLng(latitude.toDouble(), longitued.toDouble())
+    }
+}
+
+data class BackendNotifications(
+    var type:String,
+    var subscription: String
 )
