d60aaac1ff010beeab0326580c3cbf7da1033d0c
ronniedipple <ronniedipple@gmail.com>
2020-01-22 12:49:34 -0600

    UI Testing setup

--- app/src/androidTest/java/com/example/wildfire_fixed_imports/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/wildfire_fixed_imports/MainActivityTest.kt
@@ -2,27 +2,46 @@ package com.example.wildfire_fixed_imports
 
 
 import androidx.test.filters.LargeTest
+import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
 import androidx.test.rule.GrantPermissionRule
 import androidx.test.runner.AndroidJUnit4
+import androidx.test.uiautomator.UiDevice
+import androidx.test.uiautomator.UiSelector
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
-class MainActivityTest {
+class MainActivityTests {
 
-    @Rule
-    @JvmField
-    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+    private var device : UiDevice? = null
 
-    @Rule @JvmField val grantPermissions = GrantPermissionRule.grant(
-        android.Manifest.permission.ACCESS_FINE_LOCATION,
-        android.Manifest.permission.ACCESS_COARSE_LOCATION,
-        android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    @get:Rule
+    var mainActivityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    @Before
+    fun setUp() {
+        this.device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+    }
 
     @Test
-    fun mainActivityTest() {
+    fun testFeedbackLocationPermissionDenied() {
+        val denyButton = this.device?.findObject(UiSelector().text("DENY"))
+        val permissionDeniedMessage = this.device?.findObject(UiSelector().text("Permission denied"))
+
+        denyButton!!.click()
+
+        assert(permissionDeniedMessage!!.exists())
+    }
+
+    @Test
+    fun testFeedbackLocationPermissionAllowed() {
+        val allowButton = this.device?.findObject(UiSelector().text("ALLOW"))
+        var permissionAllowedMessage = this.device?.findObject(UiSelector().text("Permission allowed"))
+        allowButton!!.click()
+        assert(permissionAllowedMessage!!.exists())
     }
 }
