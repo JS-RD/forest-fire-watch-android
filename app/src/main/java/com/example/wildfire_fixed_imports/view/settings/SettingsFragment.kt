853e4337b372ff3cb88023f626e8fd8cc6e26aee
ronnie <Ronniedipple@gmail.com>
2020-01-31 20:19:24 -0600

    force dark mode enabled

--- app/src/main/java/com/example/wildfire_fixed_imports/view/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/wildfire_fixed_imports/view/settings/SettingsFragment.kt
@@ -1,5 +1,6 @@
 package com.example.wildfire_fixed_imports.view.settings
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,25 +9,37 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.example.wildfire_fixed_imports.ApplicationLevelProvider
 import com.example.wildfire_fixed_imports.R
+import com.example.wildfire_fixed_imports.viewmodel.view_model_classes.LoginViewModel
 import com.example.wildfire_fixed_imports.viewmodel.view_model_classes.SettingsViewModel
 
 class SettingsFragment : Fragment() {
 
     private lateinit var settingsViewModel: SettingsViewModel
+    private lateinit var loginViewModel: LoginViewModel
+    private val applicationLevelProvider = ApplicationLevelProvider.getApplicaationLevelProviderInstance()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        applicationLevelProvider.bottomSheet?.visibility = View.INVISIBLE
+        applicationLevelProvider.aqiGaugeExpanded.visibility = View.INVISIBLE
+        applicationLevelProvider.drawerToggle.drawerArrowDrawable.setColor(Color.BLACK)
+
         settingsViewModel =
             ViewModelProviders.of(this).get(SettingsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_settings, container, false)
-        val textView: TextView = root.findViewById(R.id.text_slideshow)
-        settingsViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
+
         return root
+    }
+
+    override fun onDetach() {
+        applicationLevelProvider.bottomSheet?.visibility = View.VISIBLE
+        applicationLevelProvider.drawerToggle.drawerArrowDrawable.setColor(Color.WHITE)
+        super.onDetach()
     }
 }
